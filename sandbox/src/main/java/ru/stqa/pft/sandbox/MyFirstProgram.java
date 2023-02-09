22e3ac3df846ef5e664145a5dc2c7da85b57f539
Testovay <vp.elenka.a@gmail.com>
2023-02-09 17:48:27 +0300

    Добавила тест GroupCreationTests

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,20 +1,20 @@
 package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
-  public static void main(String[] args) {
-    hello("World");
-    hello("User");
-    hello("Alexie");
+    public static void main(String[] args) {
+        hello("World");
+        hello("User");
+        hello("Alexie");
 
-    Square s = new Square(5);
-    System.out.println("Площадь квардата со стороной " + s.l + " = " + s.area());
+        Square s = new Square(5);
+        System.out.println("Площадь квардата со стороной " + s.l + " = " + s.area());
 
-    Rectangle r = new Rectangle(4, 6);
-    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
-  }
+        Rectangle r = new Rectangle(4, 6);
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+    }
 
-  public static void hello (String somebody) {
-    System.out.println("Hello, " + somebody + "!!!");
-  }
+    public static void hello (String somebody) {
+        System.out.println("Hello, " + somebody + "!!!");
+    }
 
 }
