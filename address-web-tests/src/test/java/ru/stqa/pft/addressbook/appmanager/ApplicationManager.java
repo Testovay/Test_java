3e80d986c27bb2496dd0d0ef96714642abab6af6
Testovay <vp.elenka.a@gmail.com>
2023-02-18 16:49:53 +0300

    Добавлены драйверы для гугла и ff

--- address-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ address-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,20 +1,33 @@
 package ru.stqa.pft.addressbook.appmanager;
 
-import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
-
-  FirefoxDriver wd;
+  WebDriver wd;
   private ContactHelper contactHelper;
   private NavigationHelper navigationHelper;
   private GroupsHelper groupsHelper;
   private SessionHelper sessionHelper;
+  private String browser;
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
 
   public void init() {
-    wd = new FirefoxDriver();
+    if (browser.equals(BrowserType.FIREFOX)) {
+      wd = new FirefoxDriver();
+    } else if (browser.equals(BrowserType.CHROME)) {
+      wd = new ChromeDriver();
+    } else if (browser.equals(BrowserType.IE)) {
+      wd = new InternetExplorerDriver();
+    }
     wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/");
     groupsHelper = new GroupsHelper(wd);
@@ -23,15 +36,12 @@ public class ApplicationManager {
     contactHelper = new ContactHelper(wd);
     sessionHelper.login("admin", "secret");
   }
-
   public void stop() {
     wd.quit();
   }
-
   public GroupsHelper getGroupsHelper() {
     return groupsHelper;
   }
-
   public NavigationHelper getNavigationHelper() {
     return navigationHelper;
   }
