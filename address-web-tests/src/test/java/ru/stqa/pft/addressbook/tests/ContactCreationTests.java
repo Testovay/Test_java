8cda018a7a0b3bf44a1c567cfb2d923e51ab4a6e
Testovay <vp.elenka.a@gmail.com>
2023-02-27 18:40:05 +0300

    Лекция 4.1

--- address-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ address-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,14 +1,18 @@
 package ru.stqa.pft.addressbook.tests;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactDate;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 
 public class ContactCreationTests extends TestBase {
   @Test
   public void testContactCreation() {
+    //app.getNavigationHelper().goGroupsPage();
+    //if (! app.getGroupsHelper().isThereAGroup()) {
+     // app.getGroupsHelper().createGroup(new GroupData("Test", "Test11","Test111"));
+    //}  -- проверка на наличие группы
     app.getNavigationHelper().goHomePage();
     app.getContactHelper().createContact(new ContactDate("Test1", "Testovich", "Testovii", "89277465676", "test@test.com", "Moscow", "Test"), true);
   }
-
 
 }
