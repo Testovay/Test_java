3cfff96d6e2a9f06b1a79e136a3b675a01d3af96
Testovay <vp.elenka.a@gmail.com>
2023-03-29 18:33:34 +0300

    Лекция 6.4

--- address-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ address-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,12 +1,16 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactDate;
 import ru.stqa.pft.addressbook.model.Contacts;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.*;
@@ -17,16 +21,22 @@ public class ContactCreationTests extends TestBase {
   public void ensurePrecondition() {
     app.goTo().groupPage();
     if (app.group().all().size() == 0) {
-      app.group().create(new GroupData().withName("Test"));
+      app.group().create(new GroupData().withName("test1"));
     }
   }
-  @Test
-  public void testContactCreation() {
+  @DataProvider
+  public Iterator<Object[]> validContacts() {
+    File photo = new File("src/test/resources/stru.png");
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[] {new ContactDate().withFirstname("Test1").withMiddlename("Testovich1").withLastname("Testovii1").withMobile("89277465676").withEmail("test@test1.com").withAddress("Moscow1").withGroup("test1").withPhoto(photo)});
+    list.add(new Object[] {new ContactDate().withFirstname("Test2").withMiddlename("Testovich2").withLastname("Testovii2").withMobile("89277465676").withEmail("test@test2.com").withAddress("Moscow2").withGroup("test1").withPhoto(photo)});
+    list.add(new Object[] {new ContactDate().withFirstname("Test3").withMiddlename("Testovich3").withLastname("Testovii3").withMobile("89277465676").withEmail("test@test3.com").withAddress("Moscow3").withGroup("test1").withPhoto(photo)});
+    return list.iterator();
+  }
+  @Test(dataProvider = "validContacts")
+  public void testContactCreation(ContactDate contact) {
     app.goTo().homePage();
     Contacts before = app.contact().all();
-    File photo = new File("src/test/resources/stru.png");
-    ContactDate contact = new ContactDate()
-            .withFirstname("Test").withMiddlename("Testovich").withLastname("Testovii").withMobile("89277465676").withEmail("test@test.com").withAddress("Moscow").withGroup("Test").withPhoto(photo);
     app.contact().create(contact, true);
     assertThat(app.contact().count(), equalTo(before.size() + 1));
     Contacts after = app.contact().all();
