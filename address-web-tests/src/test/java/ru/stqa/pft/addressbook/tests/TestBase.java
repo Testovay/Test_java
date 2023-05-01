cd8dedf6a84ef99b2aa4e6fab2d2b2bc82316676
Testovay <vp.elenka.a@gmail.com>
2023-05-01 13:24:17 +0300

    Лекция 3.10

--- address-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ address-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
@@ -8,7 +9,8 @@ import static org.openqa.selenium.remote.BrowserType.FIREFOX;
 
 public class TestBase {
 
-  protected static final ApplicationManager app = new ApplicationManager(CHROME);
+  protected static final ApplicationManager app
+          = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
   @BeforeSuite
   public void setUp() throws Exception {
